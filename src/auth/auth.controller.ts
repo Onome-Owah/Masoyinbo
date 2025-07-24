bd851666210f7759fcdda594b38d6e8a3a9f999d
Amuchayugochukw <onomeowah@gmail.com>
2025-07-24 07:52:26 +0100

    feat: implemented the sign up route handler

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Signup_dto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
- 
+  @Post('signup')
+  async signup(@Body() dto: Signup_dto) {
+    return this.authService.signup(dto);
+  }
 }
