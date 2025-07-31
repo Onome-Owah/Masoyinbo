d1aad0048af40efd6a0456375bcf6b594cd2517b
Amuchayugochukw <onomeowah@gmail.com>
2025-07-31 14:55:11 +0100

    feat:onboard user endpoint

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { Create_Admin_Dto } from './dto/create-admin.dto';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
+
+  @Post('onboard-admin')
+  @ApiBearerAuth()
+  // @UseGuards(AuthGuard('jwt'), SuperAdminGuard)
+  create(@Body() dto: Create_Admin_Dto, @Req() req) {
+    return this.adminService.onboardAdmin(dto);
+  }
 }
